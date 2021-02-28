bebd115f366874fb5a8ceb66e4fda76457180ffc
Alejo Acevedo <ale.acevedo@live.com.ar>
2021-02-28 21:33:15 +0000

    feat: implement mobile  options

--- src/containers/MainLayout.jsx
+++ src/containers/MainLayout.jsx
@@ -19,6 +19,7 @@ const MainLayout = ({ children, ...props }) => {
         <Content
           style={{
             margin: "24px 16px 24px",
+            display: "flex"
           }}
         >
           <div className="site-layout-background multiform-container">
