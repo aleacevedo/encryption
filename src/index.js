59a7e08c279b59d587c661c038002f2be84e5cbf
Alejo Acevedo <ale.acevedo@live.com.ar>
2021-03-17 16:34:00 -0300

    fix: remove initial values (#3)
    * fix: remove dist folder
    
    * feat: ignore dist folder
    
    * feat: add travis config
    
    * feat: add some changes to travis
    
    * feat: add router
    
    * feat: fix some  stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import App from "./containers/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <Router>
+    <Route path="/" component={App} />
+  </Router>,
+  document.getElementById("app")
+);
